f0a42c0003c69eb584e698013aa7b0a89677b682
dizazamira <dizahazmi2016@gmail.com>
2026-06-06 21:56:42 +0700

    Migration database, Folder Api, Middleware

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
+
+             $table->string('name');
+             $table->string('email')->unique();
+             $table->timestamp('email_verified_at')->nullable();
+             $table->string('password');
+             $table->rememberToken();
+
+              $table->enum('role', ['pembeli', 'promotor', 'super_admin'])
+          ->default('pembeli');
+
+             $table->string('nik', 16)->nullable();
+             $table->text('alamat')->nullable();
+             $table->string('no_hp', 20)->nullable();
+
+             $table->boolean('is_promotor_disetujui')->default(false);
+
+             $table->timestamps();
+});
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
---
+++ database/migrations/2026_06_06_054749_create_acara_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('acara', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('id_promotor')
+                  ->constrained('users')
+                  ->onDelete('cascade');
+
+            $table->string('judul', 150);
+            $table->text('deskripsi');
+            $table->decimal('harga', 10, 2);
+            $table->string('kategori', 50);
+            $table->integer('kuota_tiket');
+            $table->string('url_poster');
+            $table->text('syarat_ketentuan');
+
+            $table->enum('status', ['pending', 'approved', 'rejected'])
+                  ->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('acara');
+    }
+};
